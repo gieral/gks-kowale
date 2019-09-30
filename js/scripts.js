7bb62c0b2f833db7047c70d932c0f554843fd813
gieral <piotr.gieraltowski@gmail.com>
2019-09-30 20:42:53 +0200

    navbar added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.navigation');
+const nav = document.querySelector('.nav-list');
 
 const handleClick = () => {
   hamburger.classList.toggle('hamburger--active');
-  nav.classList.toggle('navigation--active');
+  nav.classList.toggle('nav-list--active');
 }
 
 hamburger.addEventListener('click', handleClick);
